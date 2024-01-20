7e83fdff7c83ac174644bc574d9754f2d6c7a612
Carlos Barraza <carlosbarraza@Carloss-MacBook-Pro.local>
2024-01-20 13:05:28 -0600

    added background.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,31 @@ const petCards = document.querySelectorAll('.pet');
 petCards.forEach(item => item.addEventListener('click', showPetInfo));
 
 function showPetInfo() {
-    this.classList.toggle('m-expanded');
     const petInfo = this.querySelector('.pet_info');
     const petImage = this.querySelector('.pet_image');
+    const petName = this.querySelector('.pet_name');
+
     petInfo.classList.toggle('h-hidden');
     petImage.classList.toggle('m-expanded');
+    petName.classList.toggle('m-expanded');
+    this.classList.toggle('m-expanded');
+
+    hideOtherPets(this);
+}
+
+function hideOtherPets(currentPet) {
+    petCards.forEach((petCard) =>  {
+        if (petCard !== currentPet) {
+            if (petCard.classList.contains('m-expanded')) {
+                const petInfo = petCard.querySelector('.pet_info');
+                const petImage = petCard.querySelector('.pet_image');
+                const petName = petCard.querySelector('.pet_name');
+
+                petCard.classList.remove('m-expanded');
+                petInfo.classList.add('h-hidden');
+                petImage.classList.remove('m-expanded');
+                petName.classList.remove('m-expanded');
+            }
+        }
+    })
 }
